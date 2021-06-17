c3199b9d83c08170fb6092f9b40163b0542274dd
Chandana0297 <saichandana9712@gmail.com>
2021-06-17 08:50:12 +0530

    added database configuration

---
+++ db/connect.php
@@ -0,0 +1,17 @@
+<?php
+
+// database details
+$host = 'localhost';
+$user = 'chandana';
+$pass = 'Admin@123';
+$dbname = 'studentDetails';
+
+// create connection to database
+$connect = new mysqli($host,$user,$pass);
+
+// Check if connection is success
+if($connect->connect_error){
+    die('Connection failed: '.$connect->connect_error);
+}
+echo 'Connect Success';
+?>
